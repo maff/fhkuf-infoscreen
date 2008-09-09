32dac6d40b8ad3a14b5c890b70d0c927f293cc70
Mathias Geat <mathias@ailoo.net>
2008-09-09 09:50:44 +0200

    changed directory structure

--- index.php
+++ index.php
@@ -6,38 +6,29 @@ ini_set('display_errors', 1);
 date_default_timezone_set('Europe/Vienna');
 
 // directory setup and class loading
-set_include_path('.' . PATH_SEPARATOR . '../library/'
-. PATH_SEPARATOR . '../application/models'
-. PATH_SEPARATOR . '../application/forms'
+set_include_path('.' . PATH_SEPARATOR . 'library/'
+. PATH_SEPARATOR . 'application/models'
+. PATH_SEPARATOR . 'application/forms'
 . PATH_SEPARATOR . get_include_path());
 
 include 'Zend/Loader.php';
 Zend_Loader::registerAutoload();
-Zend_Layout::startMvc(array('layoutPath' => '../application/layouts'));
+Zend_Layout::startMvc(array('layoutPath' => 'application/layouts'));
 
 // load configuration
-$config = new Zend_Config_Ini('../application/config/config.ini', 'settings');
+$config = new Zend_Config_Ini('application/config/config.ini', 'settings');
 $registry = Zend_Registry::getInstance();
 $registry->set('config', $config);
-
-// setup database
-//$db = Zend_Db::factory($config->db);
-//Zend_Db_Table::setDefaultAdapter($db);
-//$registry->set('db', $db);
 
 // setup controller
 $frontController = Zend_Controller_Front::getInstance();
 $frontController->throwExceptions(true);
-$frontController->setControllerDirectory('../application/controllers');
+$frontController->setControllerDirectory('application/controllers');
 $frontController->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
 
 // set doctype
 $documentType = new Zend_View_Helper_Doctype();
 $documentType->doctype('XHTML1_STRICT');
-
-// setup Access Control Lists
-// $frontController->registerPlugin(new My_Plugin_Acl(Zend_Auth::getInstance(), new My_Acl()));
-
 
 $router = $frontController->getRouter();
 $router->addRoute(
