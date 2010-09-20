b87ddd9d4cb5529ef90aa8cb4862cba486d2c42c
Mathias Geat <mathias@ailoo.net>
2010-09-20 17:36:10 +0200

    update down.php content

--- down.php
+++ down.php
@@ -1,23 +1,21 @@
-<?php header('Location: http://stud.ailoo.net/raumbelegung/'); ?>
-
+<?php header("HTTP/1.1 503 Service Unavailable"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="de-DE">
 <head profile="http://gmpg.org/xfn/11">
-	<title>Service Down - FH Kufstein Raumbelegungs-Webservice</title>
+	<title>503 Service Temporarily Unavailable - FH Kufstein Raumbelegungs-Webservice</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="/raumbelegung/_res/css/style.css" />
 </head>
 <body>
 	<div class="container">
 
 		<div id="main" class="span-24">
-            <h2>Service Down</h2>
-            <p>Da es momentan Probleme mit der Datenverarbeitung gibt, ist der Service vorübergehend inaktiv.</p>
+            <h2>503 - Service Temporarily Unavailable</h2>
+            <p>Da es momentan Probleme mit der Datenverarbeitung gibt (Quelle down), ist der Service vorübergehend inaktiv.</p>
 		</div>
 			
 		<div id="footer" class="span-24">
-			<p>&copy; 2008 <a href="http://ailoo.net/">Mathias Geat</a> &mdash; <a href="http://maff.ailoo.net/projects/fh-kufstein-raumbelegung-webservice/">Info</a></p>
+			<p>&copy; 2008-2010 <a href="http://ailoo.net/">Mathias Geat</a> &mdash; <a href="http://maff.ailoo.net/projects/fh-kufstein-raumbelegung-webservice/">Info</a></p>
 		</div>
 	</div>
 
