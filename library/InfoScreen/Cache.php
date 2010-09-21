95d5fb0c78ff79144f91162e2255c20031fb79ac
Mathias Geat <mathias@ailoo.net>
2010-09-21 16:13:08 +0200

    add documentation

--- library/InfoScreen/Cache.php
+++ library/InfoScreen/Cache.php
@@ -1,6 +1,12 @@
 <?php
 class InfoScreen_Cache
 {
+    /**
+     * Get a cache instance saved in registry
+     *
+     * @param  string $key
+     * @return Zend_Cache_Core
+     */
     public static function get($key)
     {
         return Zend_Registry::get('cache.' . $key);
