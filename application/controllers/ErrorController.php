bfc2d9807336fbf13c2034ee891a5b686cd125fd
Mathias Geat <mathias@ailoo.net>
2010-09-21 16:16:36 +0200

    change namespace in error controller

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -33,7 +33,7 @@ class ErrorController extends Zend_Controller_Action
                 break;
                 
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_OTHER:
-            case Raumbelegung_Error_Handler::EXCEPTION_CATCHED_EXCEPTION:
+            case InfoScreen_Error_Handler::EXCEPTION_CATCHED_EXCEPTION:
             default:
             
                 $this->getResponse()->setRawHeader('HTTP/1.1 500 Internal Server Error');
