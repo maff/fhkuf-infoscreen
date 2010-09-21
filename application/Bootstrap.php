fa4b0ca26f30a5bb4a4fecd5ce7fb4c453ea1aff
Mathias Geat <mathias@ailoo.net>
2010-09-21 19:52:18 +0200

    init locale and routing in bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,6 +1,45 @@
 <?php
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected function _initLocale()
+    {
+        setlocale (LC_ALL, 'de_DE');
+        date_default_timezone_set('Europe/Vienna');
+    }
+
+    protected function _initRouting()
+    {
+        $this->bootstrap('FrontController');
+
+        /* @var $front Zend_Controller_Front */
+        $front = $this->getResource('FrontController');
+
+        /* @var $router Zend_Controller_Router_Interface */
+        $router = $front->getRouter();
+        $router->removeDefaultRoutes();
+
+        $router->addRoute('index',
+            new Zend_Controller_Router_Route('/',
+                array(
+                    'controller' => 'index',
+                    'action' => 'index'
+        )));
+
+        $router->addRoute('day',
+            new Zend_Controller_Router_Route('/day/*',
+                array(
+                    'controller' => 'index',
+                    'action' => 'day'
+        )));
+
+        $router->addRoute('week',
+            new Zend_Controller_Router_Route('/week/*',
+                array(
+                    'controller' => 'index',
+                    'action' => 'week'
+        )));
+    }
+
     protected function _initDomParser()
     {
         require_once('simple_html_dom.php');
