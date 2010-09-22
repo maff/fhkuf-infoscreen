ab53dd0e10ca2dd7bfb9a06544bd32171c81f67c
Mathias Geat <mathias@ailoo.net>
2010-09-22 22:37:58 +0200

    add log type hint

--- library/InfoScreen/Log.php
+++ library/InfoScreen/Log.php
@@ -1,6 +1,10 @@
 <?php
 class InfoScreen_Log
 {
+    /**
+     *
+     * @return Zend_Log
+     */
     public static function getInstance()
     {
         return Zend_Registry::getInstance()->get('log');
