70f1a624251e7644a8e426d7f3876f64ff31c1e7
Mathias Geat <mathias@ailoo.net>
2010-09-22 17:01:55 +0200

    index controller uses helper methods

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -8,11 +8,7 @@ class IndexController extends InfoScreen_Controller_Action
 
     public function dayAction()
     {
-        if($this->getRequest()->isPost()) {
-            $filterUrl = InfoScreen_Controller_Request::getFilterUrl();
-            $filterUrl = (!empty($filterUrl)) ? '/' . $filterUrl : $filterUrl;
-            $this->_redirect('/day' . $filterUrl);
-        }
+        $this->_handlePost('/day');
 
         $model = new InfoScreen_Model_Day(InfoScreen_Controller_Request::getDate());
         $model->setFilters(InfoScreen_Controller_Request::getFilters())->load();
@@ -21,13 +17,9 @@ class IndexController extends InfoScreen_Controller_Action
 
     public function weekAction()
     {
-        if($this->getRequest()->isPost()) {
-            $filterUrl = InfoScreen_Controller_Request::getFilterUrl();
-            $filterUrl = (!empty($filterUrl)) ? '/' . $filterUrl : $filterUrl;
-            $this->_redirect('/week' . $filterUrl);
-        }
+        $this->_handlePost('/week');
 
-        $model = new InfoScreen_Model_Week(InfoScreen_Controller_Request::getDate());
+        $model = new InfoScreen_Model_DaySpan_Week(InfoScreen_Controller_Request::getDate());
         $model->setFilters(InfoScreen_Controller_Request::getFilters())->load();
         $this->view->model = $model;
     }
