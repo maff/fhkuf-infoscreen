330ae94cf130accc1d6da4af6644edc17a94f43e
Mathias Geat <mathias@ailoo.net>
2010-09-22 17:44:10 +0200

    implement list API (json, xml)

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -4,44 +4,72 @@ class ApiController extends InfoScreen_Controller_Action
     public function  preDispatch()
     {
         parent::preDispatch();
+
+        if($this->getRequest()->getActionName() =='ical' && $this->_getType() == 'list') {
+            throw new Zend_Controller_Action_Exception('Not found.', 404);
+        }
+
         $this->disableLayout();
         
         if($this->getRequest()->getParam('categorized', false) === 'true') {
             $this->view->categorized = true;
         }
+
+        if($this->_getType() == 'list') {
+            $this->_helper->viewRenderer->setViewScriptPathSpec(':controller/list/:action.:suffix');
+        }
     }
 
     protected function _getModel()
     {
-        $date = InfoScreen_Controller_Request::getDate();
+        if($this->_getType() == 'list') {
+            $model = InfoScreen_Model_List::getInstance($this->_getKey());
+        } else {
+            $date = InfoScreen_Controller_Request::getDate();
 
-        switch($this->_getType()) {
-            case 'day':
-                $model = new InfoScreen_Model_DaySpan($date, 1);
-                break;
-            case 'weekend':
-                $model = new InfoScreen_Model_DaySpan_Weekend($date);
-                break;
-            default:
-                $model = new InfoScreen_Model_DaySpan_Relative($date);
-                break;
+            switch($this->_getRange()) {
+                case 'day':
+                    $model = new InfoScreen_Model_DaySpan($date, 1);
+                    break;
+                case 'weekend':
+                    $model = new InfoScreen_Model_DaySpan_Weekend($date);
+                    break;
+                default:
+                    $model = new InfoScreen_Model_DaySpan_Relative($date);
+                    break;
+            }
+
+            $model->setFilters(InfoScreen_Controller_Request::getFilters())
+                  ->load();
         }
 
-        $model->setFilters(InfoScreen_Controller_Request::getFilters())
-              ->load();
-        
         return $model;
     }
 
     protected function _getType()
     {
-        $type = $this->getRequest()->getParam('type', false);
+        return $this->_getRequestValue('type', array('data', 'list'), 'data');
+    }
 
-        if(in_array($type, array('day', 'relative', 'weekend'))) {
-            return $type;
+    protected function _getRange()
+    {
+        return $this->_getRequestValue('range', array('day', 'relative', 'weekend'), 'relative');
+    }
+
+    protected function _getKey()
+    {
+        return $this->_getRequestValue('key', array('class', 'lector', 'room'), 'class');
+    }
+
+    protected function _getRequestValue($key, array $allowed, $default)
+    {
+        $value = $this->getRequest()->getParam($key, false);
+
+        if(in_array($value, $allowed)) {
+            return $value;
         }
 
-        return 'relative';
+        return $default;
     }
 
     public function indexAction()
