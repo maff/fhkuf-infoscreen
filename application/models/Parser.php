0d876c3fc4c653f4bbf7a25b91b4768ab2ce8549
Mathias Geat <mathias@ailoo.net>
2010-09-22 20:15:48 +0200

    cosmetics

--- application/models/Parser.php
+++ application/models/Parser.php
@@ -39,18 +39,18 @@ class InfoScreen_Model_Parser
 
         $this->_data = array();
         foreach($dom->find('div.appointment') as $element) {
-            $appointment = array();
-            $appointment['date'] = $this->_date;
-            $appointment['startTime'] = @$element->find('td.appointmentDate', 0)->plaintext;
-            $appointment['endTime'] = @$element->find('td.appointmentDate', 1)->plaintext;
-            $appointment['class'] = @$element->find('td.appointmentDate', 2)->plaintext;
-            $appointment['group'] = @$element->find('td.appointmentDate', 3)->plaintext;
-            $appointment['description'] = @$element->find('span.appointmentText', 0)->plaintext;
-            $appointment['lector'] = @$element->find('span.appointmentLektor', 0)->plaintext;
-            $appointment['room'] = @$element->find('td.appointmentRaum > div', 0)->plaintext;
-            $appointment['info'] = @$element->find('div.appointmentInfo span', 0)->plaintext;
+            $htmldata = array();
+            $htmldata['date'] = $this->_date;
+            $htmldata['startTime'] = @$element->find('td.appointmentDate', 0)->plaintext;
+            $htmldata['endTime'] = @$element->find('td.appointmentDate', 1)->plaintext;
+            $htmldata['class'] = @$element->find('td.appointmentDate', 2)->plaintext;
+            $htmldata['group'] = @$element->find('td.appointmentDate', 3)->plaintext;
+            $htmldata['description'] = @$element->find('span.appointmentText', 0)->plaintext;
+            $htmldata['lector'] = @$element->find('span.appointmentLektor', 0)->plaintext;
+            $htmldata['room'] = @$element->find('td.appointmentRaum > div', 0)->plaintext;
+            $htmldata['info'] = @$element->find('div.appointmentInfo span', 0)->plaintext;
 
-            $this->_data[] = $appointment;
+            $this->_data[] = $htmldata;
         }
     }
 }
